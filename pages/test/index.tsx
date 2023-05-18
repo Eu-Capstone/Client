4c0bf41c0b35dd02a691a48325e0bf2503e94025
dianexjeong <zuphyr14@gmail.com>
2023-05-18 12:19:27 +0900

    chore: add data types

---
+++ pages/test/index.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+const Test = () => {
+  const [curIdx, setCurIdx] = useState(0);
+  const [curType, setCurType] = useState({
+    A: 0,
+    B: 0,
+    C: 0,
+    D: 0,
+    E: 0,
+    F: 0,
+  });
+
+  const [status, setStatus] = useState("5%");
+
+  return <></>;
+};
