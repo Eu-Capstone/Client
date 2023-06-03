7e2bc006495867caac804dfd974e5a5d79ac9b3a
wooseok123 <hosmimam@naver.com>
2023-06-03 22:15:57 +0900

    feat : revise url and logic

--- src/@components/organisms/ResultButton/index.tsx
+++ src/@components/organisms/ResultButton/index.tsx
@@ -1,11 +1,12 @@
 import { Flex, Space } from "~/@components/atoms";
-import { ActionButton } from "~/@components/molecules";
+import ActionButton from "~/@components/molecules/ButtonAction";
+import { types } from "~/@utils";
 
-export const ResultButton = () => {
+export const ResultButton = ({ type }: { type: keyof typeof types }) => {
   return (
     <>
       <Flex direction="column" height="" justify="flex-start">
-        <ActionButton top />
+        <ActionButton type={type} top />
         <Space margin="12px 0px 0px 0px" />
         <ActionButton center />
         <ActionButton bottom />
