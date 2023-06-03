e4bb640ef9752cebe3c80c6977cf24aea7a653cd
wooseok123 <hosmimam@naver.com>
2023-06-03 21:31:08 +0900

    feat : declare vabs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,26 @@
 import { DefaultSeo } from "next-seo";
 import DefaultSeoInfo from "next-seo.config";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
 import { RecoilRoot } from "recoil";
 import { Applayout } from "~/@components/templates/Applayout";
 import GlobalStyle from "~/@styles/GlobalStyle";
 
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (!window.Kakao.isInitialized()) {
+      window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_API);
+    }
+  }, []);
   return (
     <>
       <RecoilRoot>
+        <script defer src="https://developers.kakao.com/sdk/js/kakao.min.js" />
         <DefaultSeo {...DefaultSeoInfo} />
         <GlobalStyle />
         <Applayout>
