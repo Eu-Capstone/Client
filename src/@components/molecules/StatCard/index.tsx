06280509e20a4e3f6ffbbb18b62c70963da18e02
wooseok123 <hosmimam@naver.com>
2023-06-07 00:18:30 +0900

    chore : chore

--- src/@components/molecules/StatCard/index.tsx
+++ src/@components/molecules/StatCard/index.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import { Flex, Text } from "~/@components/atoms";
+import { Flex, Space, Text } from "~/@components/atoms";
 import palette from "~/@styles/colors";
 import { types } from "~/@utils";
 
@@ -12,15 +12,19 @@ const StatCard = ({ idx, mbti, total }: { idx: any; mbti: any; total: any }) => 
 
   const rate = Math.round((count / totalCount) * 100);
   return (
-    <Flex direction="row" justify="center" align="center" height="auto" gap={20}>
-      <Text text={idx} size={28} />
-      <Image src={`/result-${content.id}.jpg`} width={100} height={150} alt="result" />
-      <Flex align="start" justify="start" gap={4}>
-        <Text text={content.id} size={18} weight={600} />
-        <Text text={content.subtitle} size={18} weight={400} />
-        <Text text={`${rate}%`} color={palette.gray} size={16} weight={600} />
+    <>
+      <Space margin="10px 0px 0px 0px" />
+      <Flex direction="row" justify="center" align="center" height="auto" gap={20}>
+        <Text text={idx} size={28} />
+        <Image src={`/result-${content.id}.jpg`} width={100} height={125} alt="result" />
+        <Flex align="start" justify="start" gap={4}>
+          <Text text={content.id} size={18} weight={600} />
+          <Text text={content.subtitle} size={18} weight={400} />
+          <Text text={`${rate}%`} color={palette.gray} size={16} weight={600} />
+        </Flex>
       </Flex>
-    </Flex>
+      <Space margin="10px 0px 0px 0px" />
+    </>
   );
 };
 
